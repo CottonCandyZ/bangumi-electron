cd25b881e875c50b4f8a33c80b7602b412ee61a9
CottonCandyZ <zztgqk@gmail.com>
2024-06-03 16:25:07 +0800

    chore(pkg): remove autoprefixer

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,9 @@
 function App(): JSX.Element {
-  return <div>Hello</div>
+  return (
+    <div className="text-3xl flex justify-center items-center h-dvh text-blue-300">
+      Hello <span className="i-mingcute-react-line"></span>
+    </div>
+  )
 }
 
 export default App
