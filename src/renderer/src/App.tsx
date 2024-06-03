0f84d8335da4607af13de8e25e5ec502fef4bea4
CottonCandyZ <zztgqk@gmail.com>
2024-06-03 17:49:28 +0800

    feat: use shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@renderer/components/ui/button'
+
 function App(): JSX.Element {
   return (
-    <div className="text-3xl flex justify-center items-center h-dvh text-blue-300">
-      Hello <span className="i-mingcute-react-line"></span>
+    <div className="text-3xl flex justify-center items-center h-full gap-2">
+      <div className="text-blue-300 flex justify-center items-center gap-2">
+        Hello <span className="i-mingcute-react-line"></span>
+      </div>
+      <Button>Button</Button>
     </div>
   )
 }
