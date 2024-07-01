26b1cc775a7901e9952a1402d7fd186192baddde
CottonCandyZ <zztgqk@gmail.com>
2024-07-01 22:46:59 +0800

    [feat]: 优化一下 hover card 的样式

--- src/renderer/src/components/carousel/subject-card-content.tsx
+++ src/renderer/src/components/carousel/subject-card-content.tsx
@@ -12,9 +12,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@renderer/components/ui/select'
+import { Separator } from '@renderer/components/ui/separator'
 import { useQuerySubjectInfo } from '@renderer/constants/hooks/api/subject'
 import { useTopListQuery } from '@renderer/constants/hooks/web/subject'
 import { sectionPath } from '@renderer/constants/types/web'
+import dayjs from 'dayjs'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useRef, useState } from 'react'
 
@@ -64,16 +66,16 @@ export default function SubjectCard({ sectionPath, index }: SubjectCardProps) {
               />
             </motion.div>
             <div
-              className={`absolute bottom-0 left-0 right-0 z-[3] flex h-12 justify-between bg-gradient-to-t from-black/50 px-3 pt-3`}
+              className={`absolute bottom-0 left-0 right-0 z-[3] flex h-12 items-end justify-between bg-gradient-to-t from-black/50 px-2 py-1`}
             >
-              <div className="mt-2 text-sm font-bold text-white"></div>
-
               <motion.div
-                className="text-2xl font-bold italic text-white"
+                className="flex items-center justify-center gap-1 font-bold text-white"
                 layoutId={`${layoutId}-score`}
               >
                 {subjectInfo.data?.rating.score.toFixed(1)}
+                <span className="i-mingcute-star-fill text-xs" />
               </motion.div>
+              {/* <div className="mt-2 text-sm font-bold text-white"></div> */}
             </div>
           </CardContent>
         </Card>
@@ -95,7 +97,7 @@ export default function SubjectCard({ sectionPath, index }: SubjectCardProps) {
       </motion.div>
       <AnimatePresence>
         {activeId?.sectionId === sectionId && activeId.id === id && (
-          // {(
+        // {(
           <motion.div
             className="absolute z-[10]"
             style={{
@@ -113,9 +115,9 @@ export default function SubjectCard({ sectionPath, index }: SubjectCardProps) {
           >
             <Card className="h-full w-full">
               <CardContent className="flex h-full flex-col p-2">
-                <section className="flex w-full flex-row items-start gap-2">
+                <section className="flex w-full flex-row items-start gap-2 p-2">
                   <motion.div
-                    className="shrink-0 basis-1/4 overflow-hidden rounded-lg"
+                    className="shrink-0 basis-1/6 overflow-hidden rounded-lg"
                     layoutId={`${layoutId}-image`}
                   >
                     <MotionImage
@@ -123,14 +125,14 @@ export default function SubjectCard({ sectionPath, index }: SubjectCardProps) {
                       src={subjectInfo.data?.images.common}
                     />
                   </motion.div>
-                  <section className="flex w-full flex-col justify-between">
+                  <section className="flex w-full flex-col justify-between gap-0.5">
                     <motion.div className="flex w-full flex-col" layoutId={`${layoutId}-header`}>
                       {subjectInfo.data ? (
                         <>
-                          <motion.h1 className="text-sm font-semibold">
+                          <motion.h1 className="text-xs font-bold">
                             {subjectInfo.data.name}
                           </motion.h1>
-                          <motion.h2 className="mt-1 text-xs">{subjectInfo.data.name_cn}</motion.h2>
+                          {/* <motion.h2 className="mt-1 text-xs">{subjectInfo.data.name_cn}</motion.h2> */}
                         </>
                       ) : (
                         <>
@@ -139,29 +141,41 @@ export default function SubjectCard({ sectionPath, index }: SubjectCardProps) {
                         </>
                       )}
                     </motion.div>
-                    {/* <div className="text-sm">
-                      {dayjs(subjectInfo.data?.date, 'YYYY-MM-DD').format('YY·M·D')}
-                    </div> */}
-                    <div
-                      className="mt-2 flex gap-2"
-                      onMouseEnter={() => setOnSelect(true)}
-                      onMouseLeave={() => setOnSelect(false)}
-                    >
-                      <Select onOpenChange={(open) => setOnSelect(open)}>
-                        <SelectTrigger className="flex-auto">
-                          <SelectValue placeholder="标记为" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="想看">想看</SelectItem>
-                          <SelectItem value="看过">看过</SelectItem>
-                          <SelectItem value="在看">在看</SelectItem>
-                          <SelectItem value="搁置">搁置</SelectItem>
-                          <SelectItem value="抛弃">抛弃</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      {/* <Button>取消</Button> */}
+                    <div className="flex h-4 items-center justify-start gap-1 font-semibold">
+                      <motion.div
+                        className="flex items-center justify-center gap-1 text-xs"
+                        layoutId={`${layoutId}-score`}
+                      >
+                        {subjectInfo.data?.rating.score.toFixed(1)}
+                        <span className="i-mingcute-star-fill mt-[2px] text-[0.6rem]" />
+                      </motion.div>
+                      <Separator
+                        orientation="vertical"
+                        className="h-[0.85rem] w-[1.5px] rounded-2xl"
+                      />
+                      <motion.div className="text-xs">
+                        {dayjs(subjectInfo.data?.date, 'YYYY-MM-DD').format('YY 年 M 月')}
+                      </motion.div>
                     </div>
                   </section>
+                  <div
+                    className="grow-0"
+                    onMouseEnter={() => setOnSelect(true)}
+                    onMouseLeave={() => setOnSelect(false)}
+                  >
+                    <Select onOpenChange={(open) => setOnSelect(open)}>
+                      <SelectTrigger className="flex-auto">
+                        <SelectValue placeholder="标记为" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="想看">想看</SelectItem>
+                        <SelectItem value="看过">看过</SelectItem>
+                        <SelectItem value="在看">在看</SelectItem>
+                        <SelectItem value="搁置">搁置</SelectItem>
+                        <SelectItem value="抛弃">抛弃</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
                 </section>
                 {/* <motion.div
                   className="text-2xl font-bold italic text-black"
@@ -169,12 +183,12 @@ export default function SubjectCard({ sectionPath, index }: SubjectCardProps) {
                 >
                   {subjectInfo.data?.rating.score.toFixed(1)}
                 </motion.div> */}
-                <div className="mt-2 flex max-h-fit flex-wrap gap-2 overflow-scroll">
+                <div className="mt-2 flex flex-wrap gap-2 overflow-auto p-2">
                   {subjectInfo.data?.tags.map((item) => (
                     <Button
                       key={item.name}
                       className="flex-auto px-1.5 text-xs"
-                      variant={'secondary'}
+                      variant={'outline'}
                     >
                       {item.name}
                     </Button>
