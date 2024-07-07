c376e9a9eb4ccb10ef9fd8b2db95e60b203e2635
CottonCandyZ <zztgqk@gmail.com>
2024-07-07 12:50:16 +0800

    [chore]: 移除 debug log

--- src/renderer/src/components/subject/summary.tsx
+++ src/renderer/src/components/subject/summary.tsx
@@ -2,7 +2,6 @@ import { Subject } from '@renderer/constants/types/subject'
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react'
 
 export default function Summary({ summary }: Pick<Subject, 'summary'>) {
-  console.log(summary)
   return (
     <OverlayScrollbarsComponent className="max-h-60 pr-2">
       <p className="whitespace-pre-wrap">{summary}</p>
