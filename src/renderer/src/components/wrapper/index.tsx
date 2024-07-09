897c20d1dc470c34f3e6b578aa05675e228b1c2e
CottonCandyZ <zztgqk@gmail.com>
2024-07-09 16:02:10 +0800

    [feat]: 简单的 indexDB 持久化

--- src/renderer/src/components/wrapper/index.tsx
+++ src/renderer/src/components/wrapper/index.tsx
@@ -1,11 +1,13 @@
 import { Toaster } from '@renderer/components/ui/sonner'
 import { ThemeProvider } from '@renderer/components/wrapper/theme-wrapper'
-import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryCache, QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { PropsWithChildren } from 'react'
 import { TooltipProvider } from '@renderer/components/ui/tooltip'
 import { toast } from 'sonner'
 import InitStateContextWrapper from '@renderer/components/wrapper/state-warpper'
+import { createIDBPersister } from '@renderer/lib/persister'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache({
@@ -18,15 +20,20 @@ const queryClient = new QueryClient({
   }),
   defaultOptions: {
     queries: {
-      staleTime: import.meta.env.DEV ? Infinity : 20 * 1000,
+      staleTime: import.meta.env.DEV ? 1000 * 60 * 60 * 5 : 20 * 1000,
       gcTime: import.meta.env.DEV ? Infinity : 60 * 1000 * 5,
       retry: 0,
     },
   },
 })
+
+const persister = createIDBPersister()
 export default function Wrapper({ children }: PropsWithChildren) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister, maxAge: 60 * 1000 * 60 * 24 }}
+    >
       <InitStateContextWrapper>
         <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
           <TooltipProvider>{children}</TooltipProvider>
@@ -34,6 +41,6 @@ export default function Wrapper({ children }: PropsWithChildren) {
         </ThemeProvider>
       </InitStateContextWrapper>
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   )
 }
