9e09340e48915c2a8e9539866d0a1ee050021996
CottonCandyZ <zztgqk@gmail.com>
2024-07-19 22:52:00 +0800

    [fix]: 移除无效的 class

--- src/renderer/src/components/subject/character/gird/index.tsx
+++ src/renderer/src/components/subject/character/gird/index.tsx
@@ -1,7 +1,6 @@
 import Item from '@renderer/components/subject/character/gird/item'
 import { Button } from '@renderer/components/ui/button'
 import { Character } from '@renderer/data/types/character'
-import { cn } from '@renderer/lib/utils'
 
 import { useState } from 'react'
 
@@ -10,7 +9,7 @@ export default function CharactersGrid({ characters }: { characters: Character[]
   const slice = fold ? 8 : characters.length
   const needFold = characters.length > 8
   return (
-    <div className={cn('relative flex flex-row gap-2', (!fold || !needFold) && 'max-h-none')}>
+    <div className="relative flex flex-row gap-2">
       <div className="grid w-full grid-cols-[repeat(auto-fill,_minmax(15rem,_1fr))] gap-3 py-2">
         {characters.slice(0, slice).map((item) => (
           <Item character={item} key={item.id} />
