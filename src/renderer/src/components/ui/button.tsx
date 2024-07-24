6f38d313f80d838d0f7a87bf9076426898dca5c3
Cotton <06-arid.zilch@icloud.com>
2024-07-25 00:14:11 +0800

    [feat]: hover 的时候就不用 shadow 了吧

--- src/renderer/src/components/ui/button.tsx
+++ src/renderer/src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         wantToWatch: "bg-[var(--want-to-watch-accent)] text-[var(--want-to-watch-accent-foreground] shadow-inner hover:bg-[var(--want-to-watch)] hover:text-[var(--want-to-watch-foreground)]",
-        watched: "border border-input bg-[var(--watched-accent)] text-[var(--watched-accent-foreground)] shadow-inner hover:bg-[var(--watched)] hover:text-[var(--watched-foreground)]",
+        watched: "border border-input bg-[var(--watched-accent)] text-[var(--watched-accent-foreground)] shadow-inner hover:shadow-none hover:bg-[var(--watched)] hover:text-[var(--watched-foreground)]",
         abandoned: "border border-primary-foreground line-through shadow-inner hover:bg-accent hover:text-accent-foreground text-muted-foreground",
         noAired: "hover:bg-accent hover:text-accent-foreground text-muted-foreground",
         onAir:
