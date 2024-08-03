a88ff49126e1ce6d770c3a9dfb201c2d95781f76
Cotton <06-arid.zilch@icloud.com>
2024-08-03 20:55:10 +0800

    [fix/mac]: 由于有红绿灯，所以把展开按钮往下移一些

--- src/renderer/src/components/nav/index.tsx
+++ src/renderer/src/components/nav/index.tsx
@@ -3,8 +3,11 @@ import { PanelNav } from '@renderer/components/nav/panel/nav'
 import NavProfile from '@renderer/components/nav/profile'
 import { Button } from '@renderer/components/ui/button'
 import { Separator } from '@renderer/components/ui/separator'
+import { client } from '@renderer/lib/client'
 import { cn } from '@renderer/lib/utils'
 import { useNavCollapsed } from '@renderer/state/panel'
+
+const platform = await client.platform({})
 
 export default function NavBar() {
   const { collapsed: navCollapsed, setCollapsed: setNavCollapsed } = useNavCollapsed(
@@ -15,6 +18,7 @@ export default function NavBar() {
       <div
         className={cn(
           'flex h-16 w-full shrink-0 items-center justify-center border-b p-2',
+          platform === 'darwin' && 'pt-6',
           !navCollapsed && 'justify-start pl-3',
         )}
       >
