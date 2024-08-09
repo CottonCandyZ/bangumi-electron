f28e9768fc51baf8cecee2d352a4bdd083839f49
CottonCandyZ <zztgqk@gmail.com>
2024-08-09 22:55:44 +0800

    [feat]: 优化搜索框逻辑
    - 增加快速删除按钮
    - 确保 type filter 在清空或修改时能正常的工作

--- src/renderer/src/components/serach/index.tsx
+++ src/renderer/src/components/serach/index.tsx
@@ -1,31 +1,48 @@
 import SearchContent from '@renderer/components/serach/content'
 import Filter from '@renderer/components/serach/filter'
 import { Button } from '@renderer/components/ui/button'
+import { cn } from '@renderer/lib/utils'
 import { searchParamAtom } from '@renderer/state/search'
 import { useAtom } from 'jotai'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Search() {
   const [searchParm, setSearchParm] = useAtom(searchParamAtom)
-  const [keyword, setKeyword] = useState(searchParm?.keyword)
+  const [keyword, setKeyword] = useState(searchParm?.keyword ?? '')
   const [typeFilter, setTypeFilter] = useState(new Set(searchParm?.filter?.type))
   useEffect(() => {
     if (
       searchParm?.keyword !== undefined &&
-      searchParm?.keyword !== '' &&
+      searchParm.keyword !== '' &&
       searchParm.filter?.tag?.length !== 0
-    )
-      setSearchParm({ keyword, filter: { nsfw: true, type: [...typeFilter] } })
+    ) {
+      setKeyword(searchParm?.keyword)
+      setSearchParm({ ...searchParm, filter: { ...searchParm?.filter, type: [...typeFilter] } })
+    }
   }, [typeFilter])
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
     <div className="flex w-full flex-col gap-5">
       {/* input */}
-      <div className="flex w-full max-w-xl items-center gap-2 rounded-xl border px-2 py-2 pl-4 focus-within:ring-1 focus-within:ring-ring">
+      <search className="group flex w-full max-w-xl items-center gap-2 rounded-xl border bg-accent px-2 py-2 pl-4 transition-all duration-300 focus-within:bg-background focus-within:ring-1 focus-within:ring-ring hover:bg-background">
         <span className="i-mingcute-search-2-line text-4xl" />
         <input
           value={keyword}
-          className="w-full bg-background focus-visible:outline-none"
+          autoFocus
+          ref={inputRef}
+          className="w-full bg-transparent focus-visible:outline-none"
           onChange={(e) => setKeyword(e.target.value)}
+        />
+        <button
+          className={cn(
+            'i-mingcute-close-circle-fill flex shrink-0 items-center justify-center text-xl text-transparent transition-all duration-300 group-focus-within:text-primary/30 group-hover:text-primary/30 group-hover:hover:text-primary/80',
+            keyword === '' &&
+              'group-focus-within:text-transparent group-hover:text-transparent group-hover:hover:text-transparent',
+          )}
+          onClick={() => {
+            setKeyword('')
+            inputRef.current?.focus()
+          }}
         />
         <Button
           className="rounded-lg"
@@ -33,7 +50,7 @@ export default function Search() {
         >
           搜索
         </Button>
-      </div>
+      </search>
       <Filter type={{ typeFilter, setTypeFilter }} />
 
       {searchParm && <SearchContent searchParm={searchParm} />}
