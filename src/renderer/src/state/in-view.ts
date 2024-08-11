f4f7896565d2a0f3ca8e7b3d59e3e8644737c506
Cotton <06-arid.zilch@icloud.com>
2024-08-11 12:14:42 +0800

    [fix]: 首次进入 subject 页面时 header 动画闪烁
    原因在于 cover image in view default 值
    false -> true

--- src/renderer/src/state/in-view.ts
+++ src/renderer/src/state/in-view.ts
@@ -2,4 +2,4 @@ import { atom } from 'jotai'
 
 export const collectionBoxInViewAtom = atom(false)
 
-export const subjectCoverImageInViewAtom = atom(false)
+export const subjectCoverImageInViewAtom = atom(true)
