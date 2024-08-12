055932084a27113a0291e87b5d29eb43c2953cef
CottonCandyZ <zztgqk@gmail.com>
2024-08-12 16:13:43 +0800

    [feat]: 让我们添加一些好看的动画

--- src/renderer/src/components/base/hover-card/content.tsx
+++ src/renderer/src/components/base/hover-card/content.tsx
@@ -3,6 +3,7 @@ import { UI_CONFIG } from '@renderer/config'
 import { cn } from '@renderer/lib/utils'
 import { hoverCardOpenAtomAction, triggerClientRectAtom } from '@renderer/state/hover-card'
 import { Align, Side } from '@renderer/type/ui'
+import { motion } from 'framer-motion'
 import { useAtomValue, useSetAtom } from 'jotai'
 import { HTMLProps, PropsWithChildren, useLayoutEffect, useRef, useState } from 'react'
 
@@ -29,28 +30,31 @@ export default function HoverCardContent({
   const triggerClientRect = useAtomValue(triggerClientRectAtom)
   const ref = useRef<HTMLDivElement>(null)
   const [transition, setTransition] = useState({ X: 0, Y: 0 })
+  const [position, setPosition] = useState<Record<'X' | 'Y', number> | null>(null)
   useLayoutEffect(() => {
     if (!triggerClientRect || !ref.current) return
-    const position = calcPos({
+    const c = calcPos({
       margin,
       collisionPadding,
       align,
       trigger: triggerClientRect,
       content: ref.current.getBoundingClientRect(),
     })
-    setTransition({ ...position })
-    isBottom && isBottom(position.bottom)
+    if (position === null) setPosition({ ...c })
+    else setTransition({ X: c.X - position.X, Y: c.Y - position.Y })
+    isBottom && isBottom(c.bottom)
     const ob = new ResizeObserver(() => {
       if (!ref.current) return
-      const position = calcPos({
+      const c = calcPos({
         margin,
         collisionPadding,
         align,
         trigger: triggerClientRect,
         content: ref.current.getBoundingClientRect(),
       })
-      setTransition({ ...position })
-      isBottom && isBottom(position.bottom)
+      if (position === null) setPosition({ ...c })
+      else setTransition({ X: c.X - position.X, Y: c.Y - position.Y })
+      isBottom && isBottom(c.bottom)
     })
     ob.observe(ref.current)
     return () => {
@@ -60,17 +64,28 @@ export default function HoverCardContent({
   const setOpen = useSetAtom(hoverCardOpenAtomAction)
 
   return (
-    <div
+    <motion.div
       ref={ref}
       className={cn(
         'fixed z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none',
         className,
       )}
-      style={{ top: 0, left: 0, transform: `translate(${transition.X}px, ${transition.Y}px)` }}
+      animate={{
+        opacity: [0, 1],
+        translateX: transition.X,
+        translateY: transition.Y,
+      }}
+      transition={{
+        duration: 0.2,
+      }}
+      style={{
+        top: position?.Y ?? 0,
+        left: position?.X ?? 0,
+      }}
       onMouseEnter={() => setOpen(true)}
       onMouseLeave={() => setOpen(false)}
     >
       {children}
-    </div>
+    </motion.div>
   )
 }
