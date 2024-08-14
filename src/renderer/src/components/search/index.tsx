9dd68ab1269ab5e8924319195e51a1e615f84722
CottonCandyZ <zztgqk@gmail.com>
2024-08-14 21:37:55 +0800

    [feat]: filter button

--- src/renderer/src/components/search/index.tsx
+++ src/renderer/src/components/search/index.tsx
@@ -2,6 +2,7 @@ import SearchContentWrapper from '@renderer/components/search/content-wrapper'
 import FilterButton from '@renderer/components/search/filter-button'
 import SearchInput from '@renderer/components/search/input'
 import SubjectTypeFilter from '@renderer/components/search/type-filter'
+import { Separator } from '@renderer/components/ui/separator'
 
 export default function Search() {
   return (
@@ -9,8 +10,9 @@ export default function Search() {
       <div>
         <SearchInput />
       </div>
-      <div className="flex items-center gap-2 px-10">
+      <div className="grid grid-cols-[minmax(auto,_max-content)_min-content_min-content] gap-2 px-10">
         <SubjectTypeFilter />
+        <Separator orientation="vertical" className="border" />
         <FilterButton />
       </div>
       <SearchContentWrapper />
