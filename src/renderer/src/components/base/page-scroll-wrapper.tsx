d228946bf7b050dfbb3de2f7925cdb46491cebc0
CottonCandyZ <zztgqk@gmail.com>
2024-08-19 16:54:10 +0800

    [fix]: scroll position init 导致背景闪烁
    原先的解决方案非常暴力：
    在 re-render 时 直接 重设 scroll position
    
    可是我们都知道在 render 里直接 setState 是非常危险的行为，而且还是全局 state，由于它是 state 部分渲染还未完成之前就设置，会导致 react 抱怨。
    那么此次就是来去除这个 set，同时修复闪烁问题。
    闪烁来自于首次渲染，那么实际上和 init value 有关，若参照官方的做法使用 useHydrateAtoms 和 Provider 会导致无法在 path 变时重设 init value，按照 react 的做法应该是给个新的 key，但是当成一个新组件的代价太大了，同时 overlay scroll bar 还有 init time，就导致了更严重的 bug。
    能 re-render 我们就 re-render 吧，故有此次通过修改使用时判断 init render 的做法。
    看上去不是很优雅，但是，饶了我吧。

--- src/renderer/src/components/base/page-scroll-wrapper.tsx
+++ src/renderer/src/components/base/page-scroll-wrapper.tsx
@@ -3,7 +3,6 @@ import { scrollCache } from '@renderer/state/global-var'
 import { mainPanelScrollPositionAtom, updateMainScrollPositionAtom } from '@renderer/state/scroll'
 import { useAtomValue, useSetAtom } from 'jotai'
 import { OverlayScrollbars } from 'overlayscrollbars'
-
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react'
 import { PropsWithChildren, useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
@@ -19,12 +18,8 @@ export default function PageScrollWrapper({
   const { pathname } = useLocation()
   const [instance, setInstance] = useState<OverlayScrollbars | null>(null)
   const setScrollPosition = useSetAtom(mainPanelScrollPositionAtom)
-  if (pathname.includes('subject'))
-    setScrollPosition(
-      scrollCache.get(pathname) ??
-        (pathname.includes('subject') ? UI_CONFIG.SUBJECT_INIT_SCROLL : initScrollTo),
-    )
   const updateScrollPosition = useAtomValue(updateMainScrollPositionAtom)
+
   useEffect(() => {
     instance?.elements().viewport.scrollTo({
       top: updateScrollPosition.position,
--- src/renderer/src/components/subject/background.tsx
+++ src/renderer/src/components/subject/background.tsx
@@ -1,5 +1,9 @@
+import { UI_CONFIG } from '@renderer/config'
+import { scrollCache } from '@renderer/state/global-var'
 import { mainPanelScrollPositionAtom } from '@renderer/state/scroll'
 import { useAtomValue } from 'jotai'
+import { useRef } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const scrollRange = 500
 const init = (top: number) => {
@@ -8,7 +12,16 @@ const init = (top: number) => {
 
 export default function SubjectBackground() {
   const scrollPosition = useAtomValue(mainPanelScrollPositionAtom)
-  const percent = init(scrollPosition)
+  const isFirst = useRef(false)
+  const { pathname } = useLocation()
+  // 这么写很恶心对吧，因为我无法科学的为 scrollPosition 设定 condition 的 init value,
+  // 如果使用其推荐的方案 https://github.com/pmndrs/jotai/discussions/1462，不得不将 scroll wrapper 整个重渲染
+  // 会导致换页面的时候有一个慢半拍的 scroll 的动作，当然这责任全在 overlay scroll bar
+  // 后面有机会还是用原生的 scroll bar 吧，可以少不少坑
+  const value = isFirst
+    ? (scrollCache.get(pathname) ?? UI_CONFIG.SUBJECT_INIT_SCROLL)
+    : scrollPosition
+  const percent = init(value)
 
   return (
     <div
