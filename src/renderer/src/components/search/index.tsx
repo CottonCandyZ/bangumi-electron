865f0c720d9fd54bded95d6b9c2d595e2cc501b4
Cotton <06-arid.zilch@icloud.com>
2024-08-24 22:08:45 +0800

    [fix]: 多了一列
    本来准备放分隔的 但是有一个问题
    https://stackoverflow.com/questions/32802202/how-to-center-a-flex-container-but-left-align-flex-items/32811002#32811002
    然后我感觉还是这样比较好
    
    另外删除多余的一列

--- src/renderer/src/components/search/index.tsx
+++ src/renderer/src/components/search/index.tsx
@@ -9,7 +9,7 @@ export default function Search() {
       <div>
         <SearchInput />
       </div>
-      <div className="grid grid-cols-[minmax(auto,_max-content)_min-content_min-content] gap-2 px-3">
+      <div className="grid grid-cols-[minmax(auto,_max-content)_min-content] gap-2 px-3">
         <SubjectTypeFilter />
         <FilterButton />
       </div>
