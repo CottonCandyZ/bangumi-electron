f81ec064b7741dfc14d0cb555235cf373a284a68
Cotton <06-arid.zilch@icloud.com>
2024-09-23 00:11:07 +0800

    [lint]: add ts and react-hook rule
    居然一直没有添加，加上并做一些 lint 修复

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier',
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
