fcd23ad8b06659018d26f33161e42f96d4499926
CottonCandyZ <zztgqk@gmail.com>
2024-09-26 13:56:04 +0800

    [fix(ui/条目/书籍)]: 单行本在图片缺失时能正常显示

--- src/renderer/src/modules/subject/tankobon/index.tsx
+++ src/renderer/src/modules/subject/tankobon/index.tsx
@@ -5,6 +5,7 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@renderer/components/ui
 import { useQueryRelatedSubjects } from '@renderer/data/hooks/api/subject'
 import { SubjectId } from '@renderer/data/types/bgm'
 import { RelatedSubject } from '@renderer/data/types/subject'
+import { isEmpty } from '@renderer/lib/utils/string'
 import { Link } from 'react-router-dom'
 
 export function Tankobon({ subjectId }: { subjectId: SubjectId }) {
@@ -14,13 +15,13 @@ export function Tankobon({ subjectId }: { subjectId: SubjectId }) {
   })
   const relatedSubjects = relatedSubjectsQuery.data
   const tankobon = relatedSubjects?.get('单行本')
-  if (!relatedSubjects) return <TankobonSkeleton num={7} />
+  if (!relatedSubjects) return <TankobonSkeleton num={5} />
   if (tankobon === undefined) return null
 
   return (
     <div className="flex flex-col gap-2">
       <h2 className="shrink-0 text-2xl font-medium">单行本</h2>
-      <div className="flex flex-row flex-wrap gap-1.5">
+      <div className="flex flex-row flex-wrap gap-2">
         {tankobon.map((item) => (
           <Item item={item} key={item.id} />
         ))}
@@ -33,11 +34,15 @@ function Item({ item }: { item: RelatedSubject }) {
   return (
     <Tooltip key={item.id} delayDuration={0}>
       <TooltipTrigger>
-        <Link to={`/subject/${item.id}`} unstable_viewTransition>
-          <Image
-            imageSrc={item.images.small}
-            className="size-20 overflow-hidden rounded-xl shadow transition-all duration-150 hover:shadow-xl"
-          />
+        <Link to={`/subject/${item.id}`}>
+          {!isEmpty(item.images.small) ? (
+            <Image
+              imageSrc={item.images.small}
+              className="size-20 overflow-hidden rounded-xl border shadow transition-all duration-150 hover:shadow-xl"
+            />
+          ) : (
+            <div className="size-20 rounded-xl border bg-accent shadow transition-all duration-150 hover:shadow-xl" />
+          )}
         </Link>
       </TooltipTrigger>
       <TooltipContent>
@@ -52,11 +57,11 @@ function TankobonSkeleton({ num }: { num: number }) {
   return (
     <div className="flex flex-col gap-2">
       <h2 className="shrink-0 text-2xl font-medium">单行本</h2>
-      <div className="flex flex-row flex-wrap gap-1.5">
+      <div className="flex flex-row flex-wrap gap-2">
         {Array(num)
           .fill(0)
           .map((_, index) => (
-            <Skeleton key={index} className="size-20 rounded-xl shadow" />
+            <Skeleton key={index} className="size-20" />
           ))}
       </div>
     </div>
