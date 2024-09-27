b83c36ea47f5ce6484db92b442ffa6581a6a8d14
Cotton <06-arid.zilch@icloud.com>
2024-09-27 23:53:57 +0800

    [fix(ui/条目/人物/声优)]: image 判空

--- src/renderer/src/modules/main/subject/character/gird/actor.tsx
+++ src/renderer/src/modules/main/subject/character/gird/actor.tsx
@@ -1,6 +1,7 @@
 import { Image } from '@renderer/components/image/image'
 import { Actor } from '@renderer/data/types/character'
 import { getCharacterAvatarURL } from '@renderer/lib/utils/data-trans'
+import { isEmpty } from '@renderer/lib/utils/string'
 export function Actors({ actors, showAll = false }: { actors: Actor[]; showAll?: boolean }) {
   return (
     <div className="inline-flex w-full flex-row flex-wrap gap-2 text-sm">
@@ -8,13 +9,15 @@ export function Actors({ actors, showAll = false }: { actors: Actor[]; showAll?:
         (actor, index) =>
           (showAll || index === 0) && (
             <div key={actor.id} className="flex flex-row items-end gap-2">
-              <Image
-                imageSrc={getCharacterAvatarURL(actor.images.large)}
-                className="size-9 overflow-hidden rounded-md"
-              />
+              {!isEmpty(actor.images.large) && (
+                <Image
+                  imageSrc={getCharacterAvatarURL(actor.images.large)}
+                  className="size-9 overflow-hidden rounded-md"
+                />
+              )}
               <span className="line-clamp-1 font-jp">{actor.name}</span>
               {!showAll && actors.length > 1 && (
-                <div className="-mb-0.5 flex size-6 items-center justify-center rounded-full border pb-0.5">
+                <div className="-mb-[3px] flex size-6 items-center justify-center rounded-full border pb-0.5">
                   <div className="text-[0.7rem] text-accent-foreground">+{actors.length - 1}</div>
                 </div>
               )}
