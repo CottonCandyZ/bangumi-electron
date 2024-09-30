5b97f85aca66017b2dcddb44e6c2b845a9f42c78
Cotton <06-arid.zilch@icloud.com>
2024-09-30 20:56:24 +0800

    [fix(ui/条目)]: 统一 header 在没有 name_cn 的大小

--- src/renderer/src/modules/main/subject/header-info/header.tsx
+++ src/renderer/src/modules/main/subject/header-info/header.tsx
@@ -5,7 +5,7 @@ export function Header({ name, name_cn }: Pick<Subject, 'name' | 'name_cn'>) {
   return (
     <header>
       {isEmpty(name_cn) ? (
-        <h1 className="text-2xl font-medium">{name}</h1>
+        <h1 className="text-3xl font-medium">{name}</h1>
       ) : (
         <>
           <h1 className="text-3xl font-medium">{name_cn}</h1>
