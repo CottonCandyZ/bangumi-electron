e0ac09e7ddbf699cafc09046da58235d5f604c44
Cotton <06-arid.zilch@icloud.com>
2024-10-01 22:07:48 +0800

    [fix(ui/条目/人物)]: 确保在某些情况下能够正确的显示个数

--- src/renderer/src/modules/main/subject/character/gird/index.tsx
+++ src/renderer/src/modules/main/subject/character/gird/index.tsx
@@ -1,29 +1,46 @@
 import { Button } from '@renderer/components/ui/button'
 import { Character } from '@renderer/data/types/character'
+import { useResizeObserver } from '@renderer/hooks/use-resize'
 import { Item } from '@renderer/modules/main/subject/character/gird/item'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom'
-
-export const showNumber = 8
 
 export function CharactersGrid({ characters }: { characters: Character[] }) {
   const [fold, setFold] = useState(true)
+  const [showNumber, setShowNumber] = useState(8)
   const slice = fold ? showNumber : characters.length
   const needFold = characters.length > showNumber
   const { pathname } = useLocation()
+  const ref = useRef<HTMLDivElement>(null)
   useEffect(() => {
     setFold(true)
   }, [pathname])
+  useResizeObserver({
+    ref,
+    callback: (e) => {
+      if (
+        e.target.getBoundingClientRect().width >= 240 * 3 + 8 * 2 &&
+        e.target.getBoundingClientRect().width <= 240 * 4 + 8 * 3
+      ) {
+        setShowNumber(9)
+      } else {
+        setShowNumber(8)
+      }
+    },
+  })
 
   return (
     <div className="relative flex flex-row gap-2">
-      <div className="grid w-full grid-cols-[repeat(auto-fill,_minmax(15rem,_1fr))] gap-2 py-2">
+      <div
+        className="grid w-full grid-cols-[repeat(auto-fill,_minmax(15rem,_1fr))] gap-2"
+        ref={ref}
+      >
         {characters.slice(0, slice).map((item) => (
           <Item character={item} key={item.id} />
         ))}
       </div>
       {needFold && (
-        <div className="py-2">
+        <div>
           <Button
             variant="outline"
             className="h-full whitespace-normal rounded-xl"
