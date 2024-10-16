587372eed7baf9a0deb1ca03ed9e6f11e4c18d98
CottonCandyZ <zztgqk@gmail.com>
2024-10-16 12:34:21 +0800

    [fix(ui/pkg)]:  #1752fc 的重构导致 HC 的包错误

--- src/renderer/src/modules/wrapper/pop.tsx
+++ src/renderer/src/modules/wrapper/pop.tsx
@@ -1,7 +1,8 @@
 import { Dialog } from '@renderer/components/dialog'
-import { HoverCard } from '@renderer/components/ui/hover-card'
+
 import { Toaster } from '@renderer/components/ui/sonner'
 import { TooltipProvider } from '@renderer/components/ui/tooltip'
+import { HoverCard } from '@renderer/modules/hover-card'
 import { PropsWithChildren } from 'react'
 
 export function Pop({ children }: PropsWithChildren) {
