cca2f5effc4be86951129406e63cd79515b0e163
CottonCandyZ <06-arid.zilch@icloud.com>
2025-12-31 22:46:56 +0800

    fix: 给 table 更加合理的 loading

--- src/renderer/src/modules/main/subject/person/index.tsx
+++ src/renderer/src/modules/main/subject/person/index.tsx
@@ -1,10 +1,8 @@
 import { ScrollWrapper } from '@renderer/components/scroll/scroll-wrapper'
-import { Separator } from '@renderer/components/ui/separator'
-import { Skeleton } from '@renderer/components/ui/skeleton'
 import { useWebInfoBoxQuery } from '@renderer/data/hooks/web/subject'
 import { SubjectId } from '@renderer/data/types/bgm'
 import { PersonsTable } from '@renderer/modules/main/subject/person/table'
-import { Fragment } from 'react/jsx-runtime'
+import { LoaderCircle } from 'lucide-react'
 
 export function SubjectPersonTable({ subjectId }: { subjectId: SubjectId }) {
   const personsQuery = useWebInfoBoxQuery({ subjectId })
@@ -13,17 +11,9 @@ export function SubjectPersonTable({ subjectId }: { subjectId: SubjectId }) {
     <div className="h-full pr-1">
       <ScrollWrapper className="h-full w-full pr-1" options={{ scrollbars: { autoHide: 'leave' } }}>
         {persons === undefined ? (
-          <div className="flex w-full flex-col gap-1 p-2">
-            {Array(10)
-              .fill(0)
-              .map((_, index) => (
-                <Fragment key={index}>
-                  <div className="flex flex-row gap-1">
-                    <Skeleton className="h-9 w-full" />
-                  </div>
-                  {index !== 9 && <Separator />}
-                </Fragment>
-              ))}
+          <div className="flex h-full w-full items-center justify-center p-6" role="status">
+            <LoaderCircle className="text-muted-foreground h-8 w-8 animate-spin" />
+            <span className="sr-only">Loading</span>
           </div>
         ) : (
           persons.size !== 0 && <PersonsTable persons={persons} />
